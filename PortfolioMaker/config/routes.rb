fc201e87e8fb1616798a43d4b6a9d122c0eb4191
Davida Gaffney <gdavida@gmail.com>
2016-06-24 14:05:24 -0500

    changes

--- PortfolioMaker/config/routes.rb
+++ PortfolioMaker/config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  resources :projects
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-  # Example of regular route:
+  # Example of regular rxoute:
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
