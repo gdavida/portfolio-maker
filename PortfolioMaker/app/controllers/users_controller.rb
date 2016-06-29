6c62f0ef5bd11d98a2f91349eb7ff3330931c273
Davida Gaffney <gdavida@gmail.com>
2016-06-29 14:39:39 -0500

    added users

--- PortfolioMaker/app/controllers/users_controller.rb
+++ PortfolioMaker/app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :name, :phone, :github_profile, :bio, :profile_image, :crypted_password, :salt)
+      params.require(:user).permit(:email, :name, :phone, :github_profile, :bio, :profile_image, :password, :password_confirmation)
     end
 end
